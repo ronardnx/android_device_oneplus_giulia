0219ef90034c0cddde4c60224f60fbcd9a07a4ee
Ronard NX <fucklgbt@gmail.com>
2026-08-27 15:16:05 +0000

    giulia: drop Lineage eSIM policy

--- device.mk
+++ device.mk
@@ -74,10 +74,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Telephony
-PRODUCT_PACKAGES += \
-    OplusEsimSwitcher \
-    OplusEuicc
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
