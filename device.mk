e33695c3eb2ed2070b543ff8e6b86fb691b7fc0a
Ronard NX <fucklgbt@gmail.com>
2026-08-28 09:19:28 +0000

    giulia: More AOSPA bringup

--- device.mk
+++ device.mk
@@ -43,7 +43,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-voltage
+    $(LOCAL_PATH)/overlay-aospa
 
 PRODUCT_PACKAGES += \
     KeyHandlerResTarget \
